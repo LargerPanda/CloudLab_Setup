b7f406645886041237072893f0809d78d8d87c48
Be_a_panda <huayusheng0912@gmail.com>
2019-11-30 13:05:56 -0600

    change utils&m510.sh

--- fdisk/m510.sh
+++ fdisk/m510.sh
@@ -2,7 +2,7 @@
 
 echo "======= NOTICE: NO NEED TO PARTITION, JUST FORMAT AND MOUNT. ========"
 TGTDEV="nvme0n1p4"
-bash /users/yushua/CloudLab_Setup/fdisk/formatNmount.sh $TGTDEV
+bash /local/repository/fdisk/formatNmount.sh $TGTDEV
 df -h
 
 echo "======= NOTICE: YOU ARE ALL SET!!!!! ========"
