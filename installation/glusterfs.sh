07049f8139f88aac58ee25f69e648ad9c58e17fe
Be_a_panda <huayusheng0912@gmail.com>
2020-01-03 12:18:24 -0600

    add glusterfs.sh

--- installation/glusterfs.sh
+++ installation/glusterfs.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 #this script is for server node
-
-cd ~
-wget https://download.gluster.org/pub/gluster/glusterfs/6/6.1/glusterfs-6.1.tar.gz
-tar -zxvf glusterfs-6.1.tar.gz
-cd glusterfs-6.1
-#need to modify config.sub and config.guess
-./configure
-make -j
-sudo make install
-echo "/usr/local/lib" >> /etc/ld.so.conf
-sudo su
-ldconfig
-exit
-sudo /usr/local/sbin/glusterd
-sudo mkdir -p /data/glusterfs/volume1/brick1
-sudo mkfs.xfs -f /dev/nvme0n1p4
-sudo mount /dev/nvme0n1p4 /data/glusterfs/volume1/brick1
-sudo mkdir /data/glusterfs/volume1/brick1/gv1
-sudo /usr/local/sbin/gluster volume create gv1 node-2:/data/glusterfs/volume1/brick1/gv1
+#sudo /usr/local/sbin/glusterd
+#sudo mkfs.xfs -f /dev/nvme0n1p4
+#sudo mkdir -p /data/glusterfs/volume1/brick1
+#sudo mount /dev/nvme0n1p4 /data/glusterfs/volume1/brick1
+#sudo chmod -R 777 /data
+#mkdir /data/glusterfs/volume1/brick1/gv1
+#sudo /usr/local/sbin/gluster volume stop gv1
+#sudo /usr/local/sbin/gluster volume delete gv1
+sudo /usr/local/sbin/gluster volume create gv1 ${HOSTNAME:0:6}:/data/glusterfs/volume1/brick1/gv1
 sudo /usr/local/sbin/gluster volume start gv1
