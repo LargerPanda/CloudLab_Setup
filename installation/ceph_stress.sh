6d1bc2062264a4b2330611f8cf6be90b38904e28
Be_a_panda <huayusheng0912@gmail.com>
2020-04-05 10:25:34 -0500

    modify ceph_stress

--- installation/ceph_stress.sh
+++ installation/ceph_stress.sh
@@ -2,5 +2,5 @@
 sudo sgdisk --largest-new=3 /dev/sdb
 sudo partprobe /dev/sdb
 mkdir /users/yushua/stressdir
-mkfs.ext4 /dev/sdb3
+sudo mkfs.ext4 /dev/sdb3
 sudo mount /dev/sdb3 /users/yushua/stressdir/
