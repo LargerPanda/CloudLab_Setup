d631799181845daea926cb74c4ce7c7aaf8b0adb
Be_a_panda <huayusheng0912@gmail.com>
2020-06-20 23:53:44 -0500

    add luminous_mount.sh

--- installation/luminous_mount.sh
+++ installation/luminous_mount.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo rbd map ecmeta/myrbd1   ##这一步的输出结果是dev/rbd0
-sudo mkfs.xfs /dev/rbd0
 mkdir /users/yushua/cephrbd
 sudo mount /dev/rbd0 /users/yushua/cephrbd
 sudo chown -R yushua /users/yushua/cephrbd
