3b0e0586f20848b9947621b5429d681349f59ee5
Be_a_panda <huayusheng0912@gmail.com>
2020-06-20 22:14:32 -0500

    add luminous_install.sh

--- installation/luminous_install.sh
+++ installation/luminous_install.sh
@@ -6,14 +6,14 @@ sudo wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.re
 sudo sed -i '/aliyuncs/d' /etc/yum.repos.d/CentOS-Base.repo
 sudo sed -i '/aliyuncs/d' /etc/yum.repos.d/epel.repo
 
-sudo echo [ceph] > /etc/yum.repos.d/ceph.repo
-sudo echo name=ceph >> /etc/yum.repos.d/ceph.repo
-sudo echo baseurl=http://mirrors.aliyun.com/ceph/rpm-luminous/el7/x86_64/ >> /etc/yum.repos.d/ceph.repo
-sudo echo gpgcheck=0 >> /etc/yum.repos.d/ceph.repo
-sudo echo [ceph-noarch] >> /etc/yum.repos.d/ceph.repo
-sudo echo name=cephnoarch >> /etc/yum.repos.d/ceph.repo
-sudo echo baseurl=http://mirrors.aliyun.com/ceph/rpm-luminous/el7/noarch/ >> /etc/yum.repos.d/ceph.repo
-sudo echo gpgcheck=0 >> /etc/yum.repos.d/ceph.repo
+sudo sh -c 'echo [ceph] > /etc/yum.repos.d/ceph.repo'
+sudo sh -c 'echo name=ceph >> /etc/yum.repos.d/ceph.repo'
+sudo sh -c 'echo baseurl=http://mirrors.aliyun.com/ceph/rpm-luminous/el7/x86_64/ >> /etc/yum.repos.d/ceph.repo'
+sudo sh -c 'echo gpgcheck=0 >> /etc/yum.repos.d/ceph.repo'
+sudo sh -c 'echo [ceph-noarch] >> /etc/yum.repos.d/ceph.repo'
+sudo sh -c 'echo name=cephnoarch >> /etc/yum.repos.d/ceph.repo'
+sudo sh -c 'echo baseurl=http://mirrors.aliyun.com/ceph/rpm-luminous/el7/noarch/ >> /etc/yum.repos.d/ceph.repo'
+sudo sh -c 'echo gpgcheck=0 >> /etc/yum.repos.d/ceph.repo'
 
 sudo yum makecache
 sudo yum install ceph -y
